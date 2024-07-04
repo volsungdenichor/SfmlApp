8d4c2d100a416d70f237422a7e51722af24637ef
krzysiek <krzysztof.muhleisen@gmail.com>
2024-07-04 11:37:15 +0200

    Finally, some functional approach

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,7 @@
 #include "animation.hpp"
 #include "app_runner.hpp"
 #include "event_handler_t.hpp"
-#include "widget_builders.hpp"
-
-struct boid_t
-{
-    vec_t location = {};
-    vec_t velocity = {};
-    sf::Color color = sf::Color::White;
-};
+#include "vec_t.hpp"
 
 template <class T>
 T create(const action_t<T&>& init)
@@ -45,47 +38,212 @@ sf::Font load_font(const std::string& path)
     return result;
 }
 
+namespace foo
+{
+
+struct style_t
+{
+    sf::Color fill_color = sf::Color::Black;
+    sf::Color outline_color = sf::Color::White;
+    float outline_thickness = 1.F;
+};
+
+struct text_style_t
+{
+    const sf::Font* font = nullptr;
+    unsigned int font_size = 16;
+    float letter_spacing = 1.F;
+    float line_spacing = 1.F;
+};
+
+struct state_t
+{
+    style_t style;
+    text_style_t text_style;
+    sf::RenderStates render_states;
+};
+
+struct context_t
+{
+    sf::RenderTarget* target;
+};
+
+using canvas_item = std::function<state_t(const state_t&, context_t&)>;
+
+auto fill_color(const sf::Color& color, const canvas_item& item) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        new_state.style.fill_color = color;
+        item(new_state, ctx);
+        return state;
+    };
+}
+
+auto outline_color(const sf::Color& color, const canvas_item& item) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        new_state.style.outline_color = color;
+        item(new_state, ctx);
+        return state;
+    };
+}
+
+auto outline_thickness(float value, const canvas_item& item) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        new_state.style.outline_thickness = value;
+        item(new_state, ctx);
+        return state;
+    };
+}
+
+auto font(const sf::Font& value, const canvas_item& item) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        new_state.text_style.font = &value;
+        item(new_state, ctx);
+        return state;
+    };
+}
+
+void apply_style(sf::Shape& shape, const style_t& style)
+{
+    shape.setFillColor(style.fill_color);
+    shape.setOutlineColor(style.outline_color);
+    shape.setOutlineThickness(style.outline_thickness);
+}
+
+auto text(const sf::String& str)
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        sf::Text shape{};
+        shape.setString(str);
+        shape.setFillColor(state.style.fill_color);
+        shape.setOutlineColor(state.style.outline_color);
+        shape.setOutlineThickness(state.style.outline_thickness);
+        if (state.text_style.font)
+        {
+            shape.setFont(*state.text_style.font);
+        }
+        shape.setCharacterSize(state.text_style.font_size);
+        shape.setLetterSpacing(state.text_style.letter_spacing);
+        shape.setLineSpacing(state.text_style.line_spacing);
+        ctx.target->draw(shape, state.render_states);
+        return state;
+    };
+}
+
+auto rect(const vec_t& size) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        sf::RectangleShape shape(size);
+        apply_style(shape, state.style);
+        ctx.target->draw(shape, state.render_states);
+        return state;
+    };
+}
+
+auto circle(float r) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        sf::CircleShape shape(r);
+        apply_style(shape, state.style);
+        ctx.target->draw(shape, state.render_states);
+        return state;
+    };
+}
+
+auto translate(const vec_t& v, const canvas_item& item) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        new_state.render_states.transform.translate(v);
+        item(new_state, ctx);
+        return state;
+    };
+}
+
+auto scale(const vec_t& v, const canvas_item& item) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        new_state.render_states.transform.scale(v);
+        item(new_state, ctx);
+        return state;
+    };
+}
+
+auto rotate(float a, const canvas_item& item) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        new_state.render_states.transform.rotate(a);
+        item(new_state, ctx);
+        return state;
+    };
+}
+
+template <class... Args>
+auto layer(Args&&... args) -> canvas_item
+{
+    std::vector<canvas_item> items{ canvas_item(args)... };
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        state_t new_state = state;
+        for (const auto& item : items)
+        {
+            new_state = item(new_state, ctx);
+        }
+        return state;
+    };
+}
+
+auto grid(const vec_t& size, const vec_t& dist) -> canvas_item
+{
+    return [=](const state_t& state, context_t& ctx) -> state_t
+    {
+        for (int i = 0; i < size[0]; i += dist[0])
+        {
+            sf::Vertex line[2];
+            line[0].position = sf::Vector2f(i, 0);
+            line[0].color = state.style.outline_color;
+            line[1].position = sf::Vector2f(i, size[1]);
+            line[1].color = state.style.outline_color;
+            ctx.target->draw(line, 2, sf::PrimitiveType::Lines, state.render_states);
+        }
+        for (int i = 0; i < size[1]; i += dist[1])
+        {
+            sf::Vertex line[2];
+            line[0].position = sf::Vector2f(0, i);
+            line[0].color = state.style.outline_color;
+            line[1].position = sf::Vector2f(size[0], i);
+            line[1].color = state.style.outline_color;
+            ctx.target->draw(line, 2, sf::PrimitiveType::Lines, state.render_states);
+        }
+        return state;
+    };
+}
+
+}  // namespace foo
+
 void run()
 {
-    std::vector<animation<float>> animations;
-    animations.push_back(gradual(0.F, 600.F, duration_t{ 10.F }, ease::none));
-    animations.push_back(rescale(animations[0], duration_t{ 50.F }));
-    animations.push_back(ping_pong(animations[0], 5.F));
-
-    float angle = 0.F;
-    float anim_time = 0.F;
-    std::vector<boid_t> boids = { create<boid_t>(
-                                      [](auto& it)
-                                      {
-                                          it.location = vec_t(0, 0);
-                                          it.velocity = vec_t(50, 75);
-                                          it.color = sf::Color::Red;
-                                      }),
-                                  create<boid_t>(
-                                      [](auto& it)
-                                      {
-                                          it.location = vec_t(50, 1000);
-                                          it.velocity = vec_t(40, -10);
-                                          it.color = sf::Color::Green;
-                                      }),
-                                  create<boid_t>(
-                                      [](auto& it)
-                                      {
-                                          it.location = vec_t(500, 100);
-                                          it.velocity = vec_t(-40, 20);
-                                          it.color = sf::Color::Yellow;
-                                      })
-
-    };
-
     auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
 
-    static const auto render_boid
-        = [](const boid_t& b) -> widget_t { return circle(10.F) | position(b.location) | fill(b.color); };
-
-    const sf::Texture txt = load_texture(R"(/mnt/d/Users/Krzysiek/Pictures/conan.bmp)");
-    const sf::Font arial = load_font(R"(/mnt/c/Windows/Fonts/arial.ttf)");
-    const sf::Font verdana = load_font(R"(/mnt/c/Windows/Fonts/verdana.ttf)");
     event_handler_t event_handler{};
     event_handler.on_close = [](sf::RenderWindow& w) { w.close(); };
     event_handler.on_key_pressed = [&](sf::RenderWindow& w, const sf::Event::KeyEvent& e)
@@ -94,78 +252,47 @@ void run()
         {
             w.close();
         }
-
-        if (e.code == sf::Keyboard::Num1)
-        {
-            boids[0].velocity = -boids[0].velocity;
-        }
-
-        if (e.code == sf::Keyboard::Num2)
-        {
-            boids[1].velocity = -boids[1].velocity;
-        }
     };
+
+    const sf::Font arial = load_font(R"(/mnt/c/Windows/Fonts/arial.ttf)");
+    const sf::Font verdana = load_font(R"(/mnt/c/Windows/Fonts/verdana.ttf)");
+
+    float angle = 0.F;
 
     run_app(
         window,
         event_handler,
-        [&](float dt)
-        {
-            anim_time += dt * 10.F;
-            angle += dt * 50.F;
-            static const float scale = 20.F;
-            for (auto& b : boids)
-            {
-                b.location += b.velocity * dt * scale;
-
-                if (b.location.x() > window.getView().getSize().x || b.location.x() < 0)
-                {
-                    b.velocity = vec_t(-b.velocity.x(), b.velocity.y());
-                }
-                if (b.location.y() > window.getView().getSize().y || b.location.y() < 0)
-                {
-                    b.velocity = vec_t(b.velocity.x(), -b.velocity.y());
-                }
-            }
-        },
+        [&](float dt) { angle += dt * 10.F; },
         [&](sf::RenderWindow& w, float fps)
         {
-            std::vector<drawable_t> drawables;
-            drawables.push_back(circle(200) | fill(sf::Color::Yellow) | outline(sf::Color::Red) | outline_thickness(5.0));
-            drawables.push_back(
-                rect(50.F, 20.F) | fill(sf::Color::Red) | outline(sf::Color::Yellow) | position({ 100, 50 }) | rotate(45.F));
-            drawables.push_back(circle(50.F) | fill(sf::Color::Green) | position({ 200, 50 }));
-            drawables.push_back(
-                polygon({ vec_t(0, 0),   vec_t(20, 0),  vec_t(30, 10), vec_t(50, 10), vec_t(60, 0),
-                          vec_t(80, 0),  vec_t(80, 20), vec_t(70, 30), vec_t(70, 50), vec_t(80, 60),
-                          vec_t(80, 80), vec_t(60, 80), vec_t(50, 70), vec_t(30, 70), vec_t(20, 80),
-                          vec_t(0, 80),  vec_t(0, 60),  vec_t(10, 50), vec_t(10, 30), vec_t(0, 20) })
-                | position({ 200, 200 }) | rotate(angle) | fill(sf::Color::Red));
-            for (const auto& b : boids)
-            {
-                drawables.push_back(render_boid(b));
-            }
+            const auto obj = foo::layer(
+                foo::outline_color(
+                    sf::Color(64, 0, 0), foo::grid({ float(w.getSize().x), float(w.getSize().y) }, { 100.F, 100.F })),
+                foo::translate(
+                    { 1000, 600 },
+                    foo::outline_thickness(0.F, foo::fill_color(sf::Color::White, foo::text(std::to_string(fps))))),
+                foo::translate(
+                    { 200, 0 },
+                    foo::scale(
+                        { 1.F, 2.0F },
+                        foo::layer(
+                            foo::circle(50),
+                            foo::rotate(
+                                angle,
+                                foo::translate(
+                                    { 200, 200 },
+                                    foo::layer(
+                                        foo::translate({ 200, 0 }, foo::circle(50.F)),
+                                        foo::outline_color(
+                                            sf::Color::Red, foo::fill_color(sf::Color::Green, foo::rect({ 20.F, 20.F }))),
+                                        foo::fill_color(
+                                            sf::Color::Red,
+                                            foo::translate({ 100, 50 }, foo::rotate(45.F, foo::rect({ 10.F, 5.F })))))))))));
 
-            drawables.push_back(circle(200.0) | texture({ txt, sf::IntRect{ 40, 10, 200, 200 } }) | position({ 500, 100 }));
-            drawables.push_back(sprite({ txt, sf::IntRect{ 40, 10, 200, 200 } }));
-            drawables.push_back(
-                text("Hello World!", arial, 24) | outline(sf::Color::Red) | fill(sf::Color::White) | outline_thickness(1.F)
-                | position({ 1200, 400 }));
-
-            drawables.push_back(
-                text("fps = " + std::to_string(fps), verdana, 12) | bold() | italic() | fill(sf::Color::White)
-                | position({ 1200, 100 }));
-
-            for (std::size_t i = 0; i < animations.size(); ++i)
-            {
-                drawables.push_back(
-                    circle(10) | fill(sf::Color::White) | position({ animations[i](anim_time), 500.F + 50.F * i }));
-            }
-
-            for (const auto& d : drawables)
-            {
-                d(w);
-            }
+            foo::context_t ctx{ &w };
+            foo::state_t state{};
+            state.text_style.font = &verdana;
+            obj(state, ctx);
         },
         0.01F);
 }
