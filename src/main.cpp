50e3c401efd54be9d4398da2e699a3b8c10ce67e
krzysiek <krzysztof.muhleisen@gmail.com>
2024-07-05 19:42:31 +0200

    separate file

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,27 @@
 
 #include "animation.hpp"
 #include "app_runner.hpp"
+#include "canvas_item.hpp"
 #include "event_handler_t.hpp"
 #include "vec_t.hpp"
+
+template <class Range>
+using iterator_t = decltype(std::begin(std::declval<Range>()));
+
+template <class Iter>
+using iter_reference_t = typename std::iterator_traits<Iter>::reference;
+
+template <class Func, class Range, class Res = std::invoke_result_t<Func, iter_reference_t<iterator_t<Range>>>>
+auto bind(Func&& func, Range&& range) -> Res
+{
+    Res result;
+    for (auto&& item : range)
+    {
+        auto res = std::invoke(func, std::forward<decltype(item)>(item));
+        result.insert(std::end(result), std::begin(res), std::end(res));
+    }
+    return result;
+}
 
 template <class... Args>
 std::string str(Args&&... args)
@@ -35,12 +54,6 @@ std::vector<T> vec(T head, Tail&&... tail)
     return std::vector<T>{ std::move(head), std::forward<Tail>(tail)... };
 }
 
-template <class T>
-std::vector<T> repeat(T value, std::size_t count)
-{
-    return std::vector<T>(count, std::move(value));
-}
-
 sf::Texture load_texture(const std::string& path)
 {
     sf::Texture result{};
@@ -60,283 +73,6 @@ sf::Font load_font(const std::string& path)
     }
     return result;
 }
-
-namespace foo
-{
-
-struct style_t
-{
-    sf::Color fill_color = sf::Color::Black;
-    sf::Color outline_color = sf::Color::White;
-    float outline_thickness = 1.F;
-};
-
-struct text_style_t
-{
-    const sf::Font* font = nullptr;
-    unsigned int font_size = 16;
-    float letter_spacing = 1.F;
-    float line_spacing = 1.F;
-    std::uint32_t style = sf::Text::Regular;
-};
-
-struct state_t
-{
-    style_t style;
-    text_style_t text_style;
-    sf::RenderStates render_states;
-};
-
-struct context_t
-{
-    sf::RenderTarget* target;
-};
-
-using canvas_item = std::function<void(const state_t&, context_t&)>;
-using state_modifier = action_t<state_t&>;
-
-auto operator|(const state_modifier& lhs, const state_modifier& rhs) -> state_modifier
-{
-    return [=](state_t& state)
-    {
-        lhs(state);
-        rhs(state);
-    };
-}
-
-auto operator|(const canvas_item& item, const state_modifier& modifier) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        state_t new_state = state;
-        modifier(new_state);
-        item(new_state, ctx);
-    };
-}
-
-void apply_style(sf::Shape& shape, const style_t& style)
-{
-    shape.setFillColor(style.fill_color);
-    shape.setOutlineColor(style.outline_color);
-    shape.setOutlineThickness(style.outline_thickness);
-}
-
-auto modify_state(const canvas_item& item, const action_t<state_t&>& op) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        state_t new_state = state;
-        op(new_state);
-        item(new_state, ctx);
-    };
-};
-
-auto text_style(std::uint32_t value) -> state_modifier
-{
-    return [=](state_t& state) { state.text_style.style = value; };
-}
-
-auto bold() -> state_modifier
-{
-    return [=](state_t& state) { state.text_style.style |= sf::Text::Bold; };
-}
-
-auto italic() -> state_modifier
-{
-    return [=](state_t& state) { state.text_style.style |= sf::Text::Italic; };
-}
-
-auto underlined() -> state_modifier
-{
-    return [=](state_t& state) { state.text_style.style |= sf::Text::Underlined; };
-}
-
-auto blend(sf::BlendMode mode) -> state_modifier
-{
-    return [=](state_t& state) { state.render_states.blendMode = mode; };
-}
-
-auto fill_color(const sf::Color& color) -> state_modifier
-{
-    return [=](state_t& state) { state.style.fill_color = color; };
-}
-
-auto outline_color(const sf::Color& color) -> state_modifier
-{
-    return [=](state_t& state) { state.style.outline_color = color; };
-}
-
-auto outline_thickness(float value) -> state_modifier
-{
-    return [=](state_t& state) { state.style.outline_thickness = value; };
-}
-
-auto font(const sf::Font& value) -> state_modifier
-{
-    return [&](state_t& state) { state.text_style.font = &value; };
-}
-
-auto translate(const vec_t& v) -> state_modifier
-{
-    return [=](state_t& state) { state.render_states.transform.translate(v); };
-}
-
-auto scale(const vec_t& v) -> state_modifier
-{
-    return [=](state_t& state) { state.render_states.transform.scale(v); };
-}
-
-auto scale(const vec_t& v, const vec_t& pivot) -> state_modifier
-{
-    return translate(pivot) | scale(v) | translate(-pivot);
-}
-
-auto rotate(float a) -> state_modifier
-{
-    return [=](state_t& state) { state.render_states.transform.rotate(a); };
-}
-
-auto rotate(float a, const vec_t& pivot) -> state_modifier
-{
-    return translate(pivot) | rotate(a) | translate(-pivot);
-}
-
-template <class... Args>
-auto group(Args&&... args) -> canvas_item
-{
-    std::vector<canvas_item> items{ canvas_item(args)... };
-    return [=](const state_t& state, context_t& ctx)
-    {
-        for (const auto& item : items)
-        {
-            item(state, ctx);
-        }
-    };
-}
-
-auto text(const sf::String& str) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        sf::Text shape{};
-        shape.setString(str);
-        shape.setFillColor(state.style.fill_color);
-        shape.setOutlineColor(state.style.outline_color);
-        shape.setOutlineThickness(state.style.outline_thickness);
-        if (state.text_style.font)
-        {
-            shape.setFont(*state.text_style.font);
-        }
-        shape.setCharacterSize(state.text_style.font_size);
-        shape.setLetterSpacing(state.text_style.letter_spacing);
-        shape.setLineSpacing(state.text_style.line_spacing);
-        shape.setStyle(state.text_style.style);
-        ctx.target->draw(shape, state.render_states);
-    };
-}
-
-auto rect(const vec_t& size) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        sf::RectangleShape shape(size);
-        apply_style(shape, state.style);
-        ctx.target->draw(shape, state.render_states);
-    };
-}
-
-auto circle(float r) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        sf::CircleShape shape(r);
-        apply_style(shape, state.style);
-        ctx.target->draw(shape, state.render_states);
-    };
-}
-
-auto sprite(const sf::Texture& texture, const sf::IntRect& rect) -> canvas_item
-{
-    return [&texture, rect](const state_t& state, context_t& ctx)
-    {
-        sf::Sprite shape{};
-        shape.setTexture(texture);
-        shape.setTextureRect(rect);
-        ctx.target->draw(shape, state.render_states);
-    };
-}
-
-auto grid(const vec_t& size, const vec_t& dist) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        for (int i = 0; i < size[0]; i += dist[0])
-        {
-            sf::Vertex line[2];
-            line[0].position = sf::Vector2f(i, 0);
-            line[0].color = state.style.outline_color;
-            line[1].position = sf::Vector2f(i, size[1]);
-            line[1].color = state.style.outline_color;
-            ctx.target->draw(line, 2, sf::PrimitiveType::Lines, state.render_states);
-        }
-        for (int i = 0; i < size[1]; i += dist[1])
-        {
-            sf::Vertex line[2];
-            line[0].position = sf::Vector2f(0, i);
-            line[0].color = state.style.outline_color;
-            line[1].position = sf::Vector2f(size[0], i);
-            line[1].color = state.style.outline_color;
-            ctx.target->draw(line, 2, sf::PrimitiveType::Lines, state.render_states);
-        }
-    };
-}
-
-auto triangle(const std::array<vec_t, 3>& vertices) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        sf::VertexArray shape(sf::PrimitiveType::Triangles, 3);
-        for (std::size_t i = 0; i < 3; ++i)
-        {
-            shape[i].position = vertices[i];
-            shape[i].color = state.style.fill_color;
-        }
-        ctx.target->draw(shape, state.render_states);
-    };
-}
-
-auto triangle(const vec_t& a, const vec_t& b, const vec_t& c) -> canvas_item
-{
-    return triangle({ a, b, c });
-}
-
-auto polygon(const std::vector<vec_t>& vertices) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        sf::ConvexShape shape{};
-        shape.setPointCount(vertices.size());
-        for (std::size_t i = 0; i < vertices.size(); ++i)
-        {
-            shape.setPoint(i, vertices[i]);
-        }
-        apply_style(shape, state.style);
-        ctx.target->draw(shape, state.render_states);
-    };
-}
-
-auto array(const std::vector<canvas_item>& items, const vec_t& dist) -> canvas_item
-{
-    return [=](const state_t& state, context_t& ctx)
-    {
-        for (std::size_t i = 0; i < items.size(); ++i)
-        {
-            (items[i] | translate(dist * i))(state, ctx);
-        }
-    };
-}
-
-}  // namespace foo
 
 action_t<sf::RenderWindow&, float> render_scene(
     const std::function<foo::canvas_item(const vec_t&, float)>& scene_builder, const sf::Font* default_font)
@@ -412,17 +148,13 @@ void run()
                     foo::triangle({ 10, 0 }, { 20, 20 }, { 0, 20 })  //
                         | foo::translate({ 100, 500 })               //
                         | foo::fill_color(sf::Color::Red),
-                    foo::array(
-                        repeat(
-                            foo::array(
-                                { foo::circle(10) | foo::fill_color(sf::Color::Cyan),
-                                  foo::circle(10) | foo::fill_color(sf::Color::Magenta),
-                                  foo::circle(10) | foo::fill_color(sf::Color::Yellow) },
-                                { 100, 0 })
-                                | foo::outline_color(sf::Color::Black),
-                            4),
-                        { 0, 50 })
-                        | foo::translate({ 500, 0 }),
+                    foo::map(
+                        foo::distribute({ 100.F, 0.F }),
+                        { foo::circle(10) | foo::fill_color(sf::Color::Cyan),
+                          foo::circle(10) | foo::fill_color(sf::Color::Magenta),
+                          foo::circle(10) | foo::fill_color(sf::Color::Yellow) })
+                        | foo::translate({ 0, 700 }),
+                    foo::repeat(foo::distribute({ 100.F, 0.F }), foo::circle(20), 10) | foo::translate({ 0, 800 }),
                     foo::text(str("[x", acceleration, "] fps=", std::fixed, std::setprecision(1), fps))  //
                         | foo::bold()                                                                    //
                         | foo::translate({ 1000, 600 })                                                  //
